e8052576289f80dc7bc2ab02d2631de8864ceedc
danielbuva <daniel.valdecantos@gmail.com>
2023-07-12 22:05:53 -0700

    feat: add sound effects and animation to page turn

--- frontend/src/components/Layout/index.jsx
+++ frontend/src/components/Layout/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ArrowLeft from "../icons/ArrowLeft";
 import ArrowRight from "../icons/ArrowRight";
 
@@ -12,11 +13,58 @@ export function Layout({ children }) {
 }
 
 export function PageWrapper({ onPageLeft, onPageRight, children }) {
+  const [pageLeft, setPageLeft] = useState(
+    Boolean(localStorage.getItem("pageLeft")) || false
+  );
+  const [pageRight, setPageRight] = useState(
+    Boolean(localStorage.getItem("pageRight")) || false
+  );
+
+  const playSound = () => {
+    const pageTurnSound = new Audio("/PageTurn.mp3");
+    pageTurnSound.volume = 0.3;
+    pageTurnSound.play();
+  };
+
+  useEffect(() => {
+    const pageTurnLeft = setTimeout(() => {
+      setPageLeft(false);
+      localStorage.setItem("pageLeft", "");
+    }, 50);
+
+    return () => clearTimeout(pageTurnLeft);
+  }, [pageLeft]);
+
+  useEffect(() => {
+    const pageTurnRight = setTimeout(() => {
+      setPageRight(false);
+      localStorage.setItem("pageRight", "");
+    }, 50);
+
+    return () => clearTimeout(pageTurnRight);
+  }, [pageRight]);
+
   return (
     <>
-      <ArrowLeft onClick={onPageLeft} />
+      <ArrowLeft
+        className={`left-arrow ${pageLeft ? "page-turn-left" : ""}`}
+        onClick={() => {
+          setPageLeft(true);
+          localStorage.setItem("pageLeft", ": )");
+          onPageLeft();
+          playSound();
+        }}
+      />
       <div className="base-content">{children}</div>
-      <ArrowRight onClick={onPageRight} />
+      <ArrowRight
+        className={`right-arrow ${pageRight ? "page-turn-right" : ""}`}
+        onClick={() => {
+          setPageRight(true);
+          localStorage.setItem("pageRight", "( :");
+          onPageRight();
+          playSound();
+        }}
+      />
     </>
   );
 }
--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,5 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import { PageWrapper } from "../Layout";
+import { useState } from "react";
+
 function Login() {
-  return <>login</>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handlePageRight = () => {
+    navigate("/home");
+  };
+
+  const handlePageLeft = () => {
+    navigate("/");
+  };
+
+  return (
+    <PageWrapper onPageLeft={handlePageLeft} onPageRight={handlePageRight}>
+      <div id="land-container">
+        <div id="page">
+          <h1 className="signup-header">login</h1>
+          <div className="auth-inputs">
+            <input
+              className="auth-input"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+            <input
+              className="auth-input"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.currentTarget.value)}
+            />
+          </div>
+        </div>
+      </div>
+    </PageWrapper>
+  );
 }
 
 export default Login;
