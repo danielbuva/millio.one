aa8a61bd94a61a831bfab293f3c27f0d20f398c4
danielbuva <daniel.valdecantos@gmail.com>
2023-07-14 09:50:43 -0700

    fix: match file names with description

---
+++ frontend/src/components/Mood/MoodCheckIn/Responses/Critiqued.jsx
@@ -0,0 +1,20 @@
+export default function Critiqued() {
+  return (
+    <>
+      <h1>everyone's a critic.</h1>
+      <p>
+        Criticism comes in two flavors; criticism of self and criticism of
+        others.
+      </p>
+      <p>
+        Self critique, if done constructively and with kindness, helps you
+        learn from your mistakes and improve.
+      </p>
+      <p>But negative self talk can be defeating and demoralizing.</p>
+      <p>
+        Always remember to be impeccable with your words to yourself and
+        others.
+      </p>
+    </>
+  );
+}
