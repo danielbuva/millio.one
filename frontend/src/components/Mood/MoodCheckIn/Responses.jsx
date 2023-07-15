56e2cfeed3d0aa8e2a925b46867ad04fc2fdd896
danielbuva <daniel.valdecantos@gmail.com>
2023-07-15 12:16:29 -0700

    fix: typos

--- frontend/src/components/Mood/MoodCheckIn/Responses.jsx
+++ frontend/src/components/Mood/MoodCheckIn/Responses.jsx
@@ -1056,7 +1056,7 @@ export function Confident() {
 export function Creative() {
   return (
     <>
-      <h1>in thr flow state.</h1>
+      <h1>in the flow state.</h1>
       <p>
         Feeling creative? Then don't let us get in the way. The flow state
         of creativity can be a fickle wave. Ride it as long as possible by
--- frontend/src/components/Mood/MoodCheckIn/Responses/Creative.jsx
+++ frontend/src/components/Mood/MoodCheckIn/Responses/Creative.jsx
@@ -1,7 +1,7 @@
 export default function Creative() {
   return (
     <>
-      <h1>in thr flow state.</h1>
+      <h1>in the flow state.</h1>
       <p>
         Feeling creative? Then don't let us get in the way. The flow state
         of creativity can be a fickle wave. Ride it as long as possible by
