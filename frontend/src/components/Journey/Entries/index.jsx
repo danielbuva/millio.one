96b787e61a7ae1e980a3455cfa29e406a7eddf1a
danielbuva <daniel.valdecantos@gmail.com>
2023-07-15 18:38:01 -0700

    refactor: create entries component

---
+++ frontend/src/components/Journey/Entries/index.jsx
@@ -0,0 +1,121 @@
+import Feeling0 from "../../icons/Moods/Feeling0";
+import Feeling1 from "../../icons/Moods/Feeling1";
+import Feeling2 from "../../icons/Moods/Feeling2";
+import Feeling3 from "../../icons/Moods/Feeling2";
+import Feeling4 from "../../icons/Moods/Feeling4";
+import Moon from "../../icons/Entry/Moon";
+import Sun from "../../icons/Entry/Sun";
+
+import { Link } from "react-router-dom";
+
+import { time } from "../../../utils";
+
+import "./Entry.css";
+
+function Entry({
+  createdAt,
+  header,
+  icon,
+  id,
+  prompt1,
+  prompt1Ans,
+  subHeader,
+  tags,
+  type,
+}) {
+  return (
+    <div className="entry">
+      <Link to={`/${type}/${id}`}>
+        <div className="entry-header">
+          <div className="entry-header-left">
+            <div className="entry-icon">{icon}</div>
+            <p className="entry-header-text">
+              {header} <br /> <span>{subHeader}</span>
+            </p>
+          </div>
+          <span>{createdAt}</span>
+        </div>
+        <p className="entry-prompt-one">{prompt1}</p>
+        <p className="entry-prompt-one-ans">{prompt1Ans}</p>
+        <div>
+          {tags?.map((tag) => (
+            <div>{tag}</div>
+          ))}
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+function Entries({ entries }) {
+  return <div className="entries">{entries.map(getEntryByType)}</div>;
+}
+
+function getMoodIcon(feeling) {
+  switch (feeling) {
+    case "0":
+      return <Feeling0 sm />;
+    case "1":
+      return <Feeling1 sm />;
+    case "2":
+      return <Feeling2 sm />;
+    case "3":
+      return <Feeling3 sm />;
+    case "4":
+      return <Feeling4 sm />;
+    default:
+      return null;
+  }
+}
+
+function getEntryByType(entry, i) {
+  const createdAt = time(entry.createdAt);
+  switch (entry.entryType) {
+    case 0:
+      return (
+        <Entry
+          icon={<Sun />}
+          id={entry.id}
+          key={i}
+          createdAt={createdAt}
+          header="reflection"
+          // prompt1={`why do you think ${entry.Origins[0].origin} is making you feel this way?`}
+          prompt1Ans={entry.prompt1}
+          subHeader={`morning`}
+          type="morning"
+        />
+      );
+    case 1:
+      return (
+        <Entry
+          icon={<Moon />}
+          id={entry.id}
+          key={i}
+          createdAt={createdAt}
+          header="reflection"
+          prompt1={`why do you think ${entry.origin[0]} is making you feel this way?`}
+          prompt1Ans={entry.prompt1}
+          subHeader={`evening`}
+          type="evening"
+        />
+      );
+    case 2:
+      return (
+        <Entry
+          icon={getMoodIcon(entry.feeling)}
+          id={entry.id}
+          key={i}
+          createdAt={createdAt}
+          header="mood check-in"
+          prompt1={`why do you think ${entry.origin[0]} is making you feel this way?`}
+          prompt1Ans={entry.prompt1}
+          subHeader={`you felt ${entry.description[0]}`}
+          type="moon"
+        />
+      );
+    default:
+      return null;
+  }
+}
+
+export default Entries;
--- frontend/src/components/Journey/index.jsx
+++ frontend/src/components/Journey/index.jsx
@@ -4,17 +4,9 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 import { PageWrapper } from "../Layout";
-import Entry from "./Entry";
+import Entries from "./Entries";
 
-import Feeling0 from "../icons/Moods/Feeling0";
-import Feeling1 from "../icons/Moods/Feeling1";
-import Feeling2 from "../icons/Moods/Feeling2";
-import Feeling3 from "../icons/Moods/Feeling3";
-import Feeling4 from "../icons/Moods/Feeling4";
-import Moon from "../icons/Entry/Moon";
-import Sun from "../icons/Entry/Sun";
-
-import { time } from "../../utils";
+import "./Journey.css";
 
 function Journey() {
   const entries = useSelector((s) => s.journey.entries);
@@ -32,70 +24,9 @@ function Journey() {
   console.log(entries);
   return (
     <PageWrapper onPageRight={handlePageRight} onPageLeft={handlePageLeft}>
-      <div className="entries">{entries.map(getEntry)}</div>
+      <Entries entries={entries} />
     </PageWrapper>
   );
-}
-
-function getMoodIcon(feeling) {
-  switch (feeling) {
-    case "0":
-      return <Feeling0 sm />;
-    case "1":
-      return <Feeling1 sm />;
-    case "2":
-      return <Feeling2 sm />;
-    case "3":
-      return <Feeling3 sm />;
-    case "4":
-      return <Feeling4 sm />;
-    default:
-      return null;
-  }
-}
-
-function getEntry(entry, i) {
-  const createdAt = time(entry.createdAt);
-  switch (entry.entryType) {
-    case 0:
-      return (
-        <Entry
-          icon={<Sun />}
-          key={i}
-          createdAt={createdAt}
-          header="reflection"
-          // prompt1={`why do you think ${entry.Origins[0].origin} is making you feel this way?`}
-          prompt1Ans={entry.prompt1}
-          subHeader={`morninng`}
-        />
-      );
-    case 1:
-      return (
-        <Entry
-          icon={<Moon />}
-          key={i}
-          createdAt={createdAt}
-          header="reflection"
-          prompt1={`why do you think ${entry.origin[0]} is making you feel this way?`}
-          prompt1Ans={entry.prompt1}
-          subHeader={`evening`}
-        />
-      );
-    case 2:
-      return (
-        <Entry
-          icon={getMoodIcon(entry.feeling)}
-          key={i}
-          createdAt={createdAt}
-          header="mood check - in"
-          prompt1={`why do you think ${entry.origin[0]} is making you feel this way?`}
-          prompt1Ans={entry.prompt1}
-          subHeader={`you felt ${entry.description[0]}`}
-        />
-      );
-    default:
-      return null;
-  }
 }
 
 export default Journey;
