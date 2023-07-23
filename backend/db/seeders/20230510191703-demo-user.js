7a44331c385c94bc6ea17453dcced9372b0fb0ad
danielbuva <daniel.valdecantos@gmail.com>
2023-07-23 10:15:04 -0700

    feat: only have one demo user

--- backend/db/seeders/20230510191703-demo-user.js
+++ backend/db/seeders/20230510191703-demo-user.js
@@ -1,63 +1,32 @@
 "use strict";
 const bcrypt = require("bcryptjs");
 
-let options = {};
+let options = { tableName: "Users" };
 if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA;
 }
 
 module.exports = {
   up: async (queryInterface) => {
-    options.tableName = "Users";
     try {
-      await queryInterface.bulkInsert(
-        options,
-        [
-          {
-            name: "demo",
-            email: "demo@user.io",
-            hashedPassword: bcrypt.hashSync("password"),
-          },
-          {
-            name: "Bianca",
-            email: "chiefyangga@gmail.com",
-            hashedPassword: bcrypt.hashSync("696969"),
-          },
-          {
-            name: "Tanya",
-            email: "tanyamikaia@gmail.com",
-            hashedPassword: bcrypt.hashSync("696969"),
-          },
-          {
-            name: "Laurent",
-            email: "laurentlucian@gmail.com",
-            hashedPassword: bcrypt.hashSync("696969"),
-          },
-          {
-            name: "Danica",
-            email: "daniboo@gmail.com",
-            hashedPassword: bcrypt.hashSync("696969"),
-          },
-        ],
-        {}
-      );
+      await queryInterface.bulkInsert(options, [
+        {
+          id: 1,
+          name: "demo",
+          email: "demo@user.io",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+      ]);
     } catch (err) {
       console.error(err);
     }
   },
 
   down: async (queryInterface, Sequelize) => {
-    options.tableName = "Users";
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       email: {
-        [Op.in]: [
-          "demo@user.io",
-          "chiefyangga@gmail.com",
-          "tanyamikaia@gmail.com",
-          "laurentlucian@gmail.com",
-          "daniboo@gmail.com",
-        ],
+        [Op.in]: ["demo@user.io"],
       },
     });
   },
