3cfb37fbcdefd52b03e416b38fd8dd3199745a80
danielbuva <daniel.valdecantos@gmail.com>
2024-11-12 17:55:08 -0800

    fix: add gratitude prompts to production

--- backend/db/migrations/20230720170332-create-ty-prompt.js
+++ backend/db/migrations/20230720170332-create-ty-prompt.js
@@ -1,33 +1,43 @@
 "use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production" && process.env.SCHEMA) {
+  options.schema = process.env.SCHEMA;
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("TyPrompts", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      "TyPrompts",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        prompt: {
+          allowNull: false,
+          type: Sequelize.STRING(200),
+        },
+        version: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      prompt: {
-        allowNull: false,
-        type: Sequelize.STRING(200),
-      },
-      version: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("TyPrompts");
